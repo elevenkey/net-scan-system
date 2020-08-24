898b58b114758850c4b6eee410b86ebf8a45cb8e
sunzhen <sunzhen@yuanbaotech.cn>
2020-08-24 14:21:35 +0800

    添加依赖

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController, AppController2 } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule], // 导入其它module
+  imports: [UserModule, AuthModule, MongooseModule.forRoot('mongodb://localhost/test')], // 导入其它module
   controllers: [AppController, AppController2],
   providers: [AppService],
 })
