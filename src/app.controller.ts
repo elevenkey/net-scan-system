a9968c2699784fed2a0e0fb1889890ebd1e64b45
sunzhen <sunzhen@yuanbaotech.cn>
2020-08-24 17:07:46 +0800

    test mongo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,35 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import { Controller, Get, Query, Param, Injectable } from '@nestjs/common';
 import { AppService } from './app.service';
+
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 // import { Request } from 'express';
 
 @Controller('hello')
+@Injectable()
 export class AppController {
-
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, @InjectConnection() private db: Connection) {
+  }
 
   @Get('world/:id')
-  getHello(@Query('hello001') hello001: string, @Param('id') id: string): string {
-    return this.appService.getHello() + hello001 + id;
+  async getHello(@Query('hello001') hello001: string, @Param('id') id: string): Promise<string> {
+    const user = await this.db.collection('user').find();
+    const uu = []
+    while(await user.hasNext()){
+      // console.log(await user.next())
+      uu.push((await user.next()).name)
+    }
+    
+    // user.forEach((u) => {
+    //   console.log(u)
+    //   uu.push(u)
+    // })
+    
+    if (user) {
+     return this.appService.getHello() + hello001 + id + '<br>' + uu.toString();
+    } else {
+      return '找不到用户'
+    }
   }
 
 }
